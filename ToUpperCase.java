2e76c9107e8b276e5a1ae7875dce18f7efc4e017
Monica Alexandra Diaz <madiaz-2023a@corhuila.edu.co>
2025-11-03 02:02:33 -0500

    Corrected string input handling in ToUpperCase
    Fix formatting issues in ToUpperCase.java

--- ToUpperCase.java
+++ ToUpperCase.java
@@ -1,7 +1,7 @@
 public class ToUpperCase {
     public static void main(String[] args) {
         if (args.length > 0) {
-            String input = args[0];
+            String input = args[0]; 
             String output = input.toUpperCase();
             System.out.println(output);
         } else {
